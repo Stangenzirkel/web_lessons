463435601328d9ec20f431a1a92b30704f09d9a4
Yuri Buyanov <yuri.buyanov@gmail.com>
2021-03-13 23:30:11 +0300

    init

---
+++ forms/add_job.py
@@ -0,0 +1,13 @@
+from flask_wtf import FlaskForm
+from wtforms import PasswordField, BooleanField, SubmitField, StringField
+from wtforms.fields.html5 import EmailField, IntegerField, DateTimeField
+from wtforms.validators import DataRequired
+
+
+class JobForm(FlaskForm):
+    team_leader = IntegerField('Капитан', validators=[DataRequired()])
+    job = StringField('Описание', validators=[DataRequired()])
+    work_size = IntegerField('Длительность', validators=[DataRequired()])
+    collaborators = StringField('Помошники')
+    is_finished = BooleanField('Статус')
+    submit = SubmitField('Создать')
